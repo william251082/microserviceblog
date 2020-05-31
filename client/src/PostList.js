72d5947546e4feb319275deaf2636fbf3fa63fce
william <william.del.rosario@iceshop.nl>
2020-05-31 15:01:47 +0200

    Rendering Posts

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -14,11 +14,23 @@ export default () => {
         fetchPosts();
     }, []);
 
-    console.log(posts);
+    const renderedPosts = Object.values(posts).map(post => {
+        return (
+            <div
+                className="card"
+                style={{width: '30%', marginBottom: '20px' }}
+                key={post.id}
+            >
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                </div>
+            </div>
+        );
+    });
 
     return (
-        <div>
-
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {renderedPosts}
         </div>
     );
 }
