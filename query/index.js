6de924dea8c6fd9f5639daea4782a44008fd637f
william <william.del.rosario@iceshop.nl>
2020-05-31 19:33:18 +0200

    Parsing Incoming Events

--- query/index.js
+++ query/index.js
@@ -7,11 +7,42 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts/:id/comments', (req, res) => {
+const posts = {};
+// example posts data structure
+// posts === {
+//     'werqwe': {
+//         id: 'wqwe232',
+//         title: 'ygvhbjnk',
+//         comments: [
+//             id: '12fdge',
+//             content: 'comment!'
+//         ]
+//     }
+// }
 
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
 });
-app.post('/posts/:id/comments', async (req, res) => {
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
 
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+
+        posts[id] = { id, title, comments: []};
+    }
+
+    if (type === 'CommentCreated') {
+        const { id, content, postId } = data;
+
+        const post = posts[postId];
+        post.comments.push({ id, content })
+    }
+debugger;
+    console.log(posts);
+
+    res.send({});
 });
 
 app.listen(4002, () => {
