a6892c80baf73a4ff7d51e27ed1825767b1bc1b4
william <william.del.rosario@iceshop.nl>
2020-06-01 08:57:56 +0200

    Implementing event sync

--- query/index.js
+++ query/index.js
@@ -8,14 +8,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    console.log('app.get', posts);
-    res.send(posts);
-});
-app.post('/events', async (req, res) => {
-    const { type, data } = req.body;
-
-    console.log('app.post-before', posts);
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
 
@@ -40,13 +33,29 @@ app.post('/events', async (req, res) => {
         comment.content = content;
         console.log('comment updated', comment);
     }
+};
 
-    console.log('app.post', posts);
+app.get('/posts', (req, res) => {
+    console.log('app.get', posts);
+    res.send(posts);
+});
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 });
 
-app.listen(4002, () => {
-    console.log('Listening on 4002 query')
+app.listen(4002, async () => {
+    console.log('Listening on 4002 query');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+        console.log('Processing event:', event.type);
+
+        handleEvent(event.type, event.data);
+    }
 });
 
